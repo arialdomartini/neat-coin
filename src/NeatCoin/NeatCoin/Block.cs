cbb1263e8dd86e03dab28a0458c4bf04318e42b9
Arialdo Martini <arialdomartini@gmail.com>
2019-03-10 09:59:12 +0100

    Blocks can contain a string value

--- src/NeatCoin/NeatCoin/Block.cs
+++ src/NeatCoin/NeatCoin/Block.cs
@@ -1,18 +1,30 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace NeatCoin
 {
     internal class Block
     {
         private readonly DateTimeOffset _createdAt;
+        private readonly string _content;
         private readonly Hash _hash;
 
-        internal Block(DateTimeOffset createdAt, Hash hash)
+        internal Block(Hash hash, DateTimeOffset createdAt, string content)
         {
-            _createdAt = createdAt;
             _hash = hash;
+
+            _createdAt = createdAt;
+            _content = content;
         }
 
-        public string Hash => _hash.CalculateHash(_createdAt.AsString());
+        public string Hash => _hash.CalculateHash(SerializeObject());
+
+        private string SerializeObject() =>
+            JsonConvert.SerializeObject(
+                new
+                {
+                    CreatedAt = _createdAt.AsString(),
+                    Content = _content
+                });
     }
 }
--- src/NeatCoin/NeatCoinTest/BlockTest.cs
+++ src/NeatCoin/NeatCoinTest/BlockTest.cs
@@ -19,8 +19,21 @@ namespace NeatCoinTest
         public void blocks_created_in_different_moments_should_have_different_hash_values()
         {
             var now = DateTimeOffset.UtcNow;
-            var block1 = new Block(now, _hash);
-            var block2 = new Block(now.AddMilliseconds(1), _hash);
+            var block1 = new Block(_hash, now, "same content");
+            var block2 = new Block(_hash, now.AddMilliseconds(1), "same content");
+
+            var hash1 = block1.Hash;
+            var hash2 = block2.Hash;
+
+            hash1.Should().NotBe(hash2);
+        }
+
+        [Fact]
+        public void blocks_with_different_contents_should_have_different_hash_values()
+        {
+            var sameMoment = DateTimeOffset.UtcNow;
+            var block1 = new Block(_hash, sameMoment, "content1");
+            var block2 = new Block(_hash, sameMoment, "content2");
 
             var hash1 = block1.Hash;
             var hash2 = block2.Hash;
