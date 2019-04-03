8e5819e72b4f69dbf2a9e2432ab55878042d5e32
Arialdo Martini <arialdomartini@gmail.com>
2019-04-03 09:12:25 +0200

    Calculates Account's balance

--- src/NeatCoin/NeatCoin/Ledger.cs
+++ src/NeatCoin/NeatCoin/Ledger.cs
@@ -8,5 +8,12 @@ namespace NeatCoin
         {
             Transaction = transaction;
         }
+
+        public int Balance(string account)
+        {
+            if (Transaction.Sender == account) return -Transaction.Amount;
+            if(Transaction.Receiver == account) return Transaction.Amount;
+            return 0;
+        }
     }
 }
--- src/NeatCoin/NeatCoinTest/LedgerTest.cs
+++ src/NeatCoin/NeatCoinTest/LedgerTest.cs
@@ -12,8 +12,38 @@ namespace NeatCoinTest
             var sut = new Ledger(new Transaction("sender", "receiver", 10));
 
             var result = sut.Transaction;
-            
+
             result.Should().Be(new Transaction("sender", "receiver", 10));
+        }
+
+        [Fact]
+        public void can_return_sender_s_balance()
+        {
+            var sut = new Ledger(new Transaction("sender", "receiver", 10));
+
+            var result = sut.Balance("sender");
+
+            result.Should().Be(-10);
+        }
+
+        [Fact]
+        public void can_return_receiver_s_balance()
+        {
+            var sut = new Ledger(new Transaction("sender", "receiver", 10));
+
+            var result = sut.Balance("receiver");
+
+            result.Should().Be(10);
+        }
+
+        [Fact]
+        public void can_return_an_unknown_account_s_balance()
+        {
+            var sut = new Ledger(new Transaction("sender", "receiver", 10));
+
+            var result = sut.Balance("unknown account");
+
+            result.Should().Be(0);
         }
     }
 }
