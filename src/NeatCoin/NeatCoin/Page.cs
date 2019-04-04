57fea124a885a29eb6b81c1437f6ee7df32f0cec
Arialdo Martini <arialdomartini@gmail.com>
2019-04-04 15:44:23 +0200

    Pages are valid only if their hash matches difficulty

--- src/NeatCoin/NeatCoin/Page.cs
+++ src/NeatCoin/NeatCoin/Page.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using Newtonsoft.Json;
 
@@ -10,15 +11,34 @@ namespace NeatCoin
     {
         public ImmutableList<Transaction> Transactions { get; }
         public string Parent { get; }
+        public int Nonce { get; }
 
-        public string Hash =>
-            Convert.ToBase64String(
-                Encoding.UTF8.GetBytes(
-                    JsonConvert.SerializeObject(
-                        new {Transactions, Parent})));
+        public string Hash => CalculateHash();
 
-        public Page(string parent, ImmutableList<Transaction> transactions)
+        private string CalculateHash()
         {
+            using (var sha1 = new SHA1Managed())
+            {
+                return Convert.ToBase64String(
+                    sha1.ComputeHash(
+                        Encoding.UTF8.GetBytes(Serialized)));
+            }
+        }
+
+        private string Serialized =>
+            JsonConvert.SerializeObject(
+                new
+                {
+                    Transactions,
+                    Parent,
+                    Nonce
+                });
+
+        public Page(string parent, ImmutableList<Transaction> transactions) : this(parent, transactions, 0) {}
+
+        public Page(string parent, ImmutableList<Transaction> transactions, int nonce)
+        {
+            Nonce = nonce;
             Parent = parent;
             Transactions = transactions;
         }
@@ -26,5 +46,22 @@ namespace NeatCoin
         public Page(params Transaction[] transactions) : this("", ImmutableList.Create(transactions)){}
 
         public int BalanceOf(string account) => Transactions.Sum(t => t.BalanceOf(account));
+
+        public bool IsValid(int difficulty) => Hash.StartsWith(new string('0', difficulty));
+
+        public Page Validate(int difficulty)
+        {
+            for (var i = 0; i < int.MaxValue; i++)
+            {
+                var page = CloneWithNonce(i);
+                if (page.IsValid(difficulty))
+                    return page;
+            }
+
+            throw new Exception();
+        }
+
+        private Page CloneWithNonce(int nonce) =>
+            new Page(Parent, Transactions, nonce);
     }
 }
--- src/NeatCoin/NeatCoinTest/PageTest.cs
+++ src/NeatCoin/NeatCoinTest/PageTest.cs
@@ -30,5 +30,37 @@ namespace NeatCoinTest
 
             hash1.Should().NotBe(hash2);
         }
+
+        [Fact]
+        public void pages_with_different_nonces_should_have_different_hash_values()
+        {
+            var page1 = new Page("some parent", ImmutableList<Transaction>.Empty, 1);
+            var page2 = new Page("some parent", ImmutableList<Transaction>.Empty, 2);
+
+            var hash1 = page1.Hash;
+            var hash2 = page2.Hash;
+
+            hash1.Should().NotBe(hash2);
+        }
+
+        [Fact]
+        public void pages_with_hashes_not_matching_difficulty_are_not_valid()
+        {
+            var sut = new Page(new Transaction("from", "to", 42));
+
+            sut.IsValid(2).Should().Be(false);
+            sut.Hash.Should().NotStartWith("00");
+        }
+
+        [Fact]
+        public void pages_with_hashes_matching_difficulty_are_valid()
+        {
+            var page =
+                new Page(new Transaction("from", "to", 42))
+                    .Validate(2);
+
+            page.IsValid(1).Should().Be(true);
+            page.Hash.Should().StartWith("00");
+        }
     }
 }
