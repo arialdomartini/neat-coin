9b5b05808345aaf83a9ab305c3005218e4e20040
Arialdo Martini <arialdomartini@gmail.com>
2019-04-23 12:22:21 +0200

    Can calculate balance

--- src/NeatCoin/NeatCoin/Ledger.cs
+++ src/NeatCoin/NeatCoin/Ledger.cs
@@ -7,11 +7,18 @@ namespace NeatCoin
             Transaction = transaction;
         }
 
-        public Ledger Append(Transaction transaction)
-        {
-            return new Ledger(transaction);
-        }
+        public Ledger Append(Transaction transaction) =>
+            new Ledger(transaction);
 
         public Transaction Transaction { get; }
+
+        public int BalanceOf(string account)
+        {
+            if (account == Transaction.Receiver)
+                return Transaction.Amount;
+            if (account == Transaction.Sender)
+                return -Transaction.Amount;
+            return 0;
+        }
     }
 }
--- src/NeatCoin/NeatCoinTest/LedgerTest.cs
+++ src/NeatCoin/NeatCoinTest/LedgerTest.cs
@@ -9,11 +9,41 @@ namespace NeatCoinTest
         [Fact]
         public void should_contain_a_transaction()
         {
-            var ledger = new Ledger();
+            var ledger = new Ledger()
+                .Append(
+                    new Transaction("sender", "receiver", 100));
 
-            var result = ledger.Append(new Transaction("sender", "receiver", 100));
+            ledger.Transaction.Should().Be(new Transaction("sender", "receiver", 100));
+        }
 
-            result.Transaction.Should().Be(new Transaction("sender", "receiver", 100));
+        [Fact]
+        public void should_calculate_sender_s_balance()
+        {
+            var ledger = new Ledger()
+                .Append(
+                    new Transaction("sender", "receiver", 100));
+
+            ledger.BalanceOf("sender").Should().Be(-100);
+        }
+
+        [Fact]
+        public void should_calculate_receiver_s_balance()
+        {
+            var ledger = new Ledger()
+                .Append(
+                    new Transaction("sender", "receiver", 100));
+
+            ledger.BalanceOf("receiver").Should().Be(100);
+        }
+
+        [Fact]
+        public void unknown_account_s_balance_should_be_0()
+        {
+            var ledger = new Ledger()
+                .Append(
+                    new Transaction("sender", "receiver", 100));
+
+            ledger.BalanceOf("unknown account").Should().Be(0);
         }
     }
 }
